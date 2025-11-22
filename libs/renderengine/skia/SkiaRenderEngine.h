d11f271cf6f2f4c01e8a36a9dbcb2de8d85ead2a
Mashopy <eliasgheeraert@gmail.com>
2025-11-22 11:34:07 +0100

    Fix the udfps dimlayer on MediaTek HWComposer
    Right now the entire screen dither to dark except for the dim layer
    better than getting flashbanged at HBM level but still not the expected behavior.
    
    TODO: Fix this and find out what bs mtk do to not trigger this.
    
    Change-Id: I66cb2d7d2ca3d08f768c6a66521a60eaa66015aa

--- libs/renderengine/skia/SkiaRenderEngine.h
+++ libs/renderengine/skia/SkiaRenderEngine.h
@@ -41,6 +41,9 @@
 #include "filters/LinearEffect.h"
 #include "filters/LutShader.h"
 #include "filters/StretchShaderFactory.h"
+#ifdef MTK_IN_DISPLAY_FINGERPRINT
+#include "../mediatek/SkiaDitherEffect.h"
+#endif
 
 class SkData;
 
@@ -80,6 +83,9 @@ public:
     void ensureContextsCreated();
 
 protected:
+#ifdef MTK_IN_DISPLAY_FINGERPRINT
+    SkiaDitherEffect * mSkiaDitherEffect = nullptr;
+#endif
     // This is so backends can stop the generic rendering state first before cleaning up
     // backend-specific state. SkiaGpuContexts are invalid after invocation.
     void finishRenderingAndAbandonContexts();
